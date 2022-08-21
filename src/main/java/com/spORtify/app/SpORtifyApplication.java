fad4c9f580cd09f7e1e9455fe1d607115da56feb
kacxsku <kacperskurski23@gmail.com>
2022-08-21 21:22:53 +0200

    initial commit

---
+++ src/main/java/com/spORtify/app/SpORtifyApplication.java
@@ -0,0 +1,13 @@
+package com.spORtify.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpORtifyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpORtifyApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/spORtify/app/SpORtifyApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spORtify.app;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpORtifyApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
